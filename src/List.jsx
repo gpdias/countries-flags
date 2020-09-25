e33a542904d9eedfabbe6b69897accf3d626b017
Germano Paulino Dias <gpdias@yahoo.com.br>
2020-09-25 10:52:22 -0300

    add fetch data and filter search

--- src/List.jsx
+++ src/List.jsx
@@ -1,21 +1,45 @@
-import React from "react"
-import Cards from "./Cards"
-import paises from "./all"
-
-function createCards(pais) {
-return (
-    <Cards 
-    name = {pais.name}
-    flag= {pais.flag}
-    />
-); }
+import React, { useState, useEffect } from "react";
 
 function List() {
-    return(
-       <div className = "list">{paises.map(createCards)}</div> 
-    )
-        
-}
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filtered, setFiltered] = useState([]);
 
+  const API_URL = "http://restcountries.eu/rest/v2/all";
+  const getData = () => fetch(API_URL).then((res) => res.json());
+
+  useEffect(() => {
+    getData().then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
+    setFiltered(
+      data.filter((country) =>
+        country.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, data]);
+
+  return (
+    <div className="list">
+      <h1>Bandeiras do Mundo</h1>
+
+      <input
+        name="name"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <br />
+      {filtered.map((pais) => (
+        <div>
+          <h3>{pais.name}</h3>
+          <img className="Flags" src={pais.flag} alt="flag_img" />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default List;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Heading from './Heading';
-import List from './List';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import List from "./List";
 
 ReactDOM.render(
   <div>
-    <Heading />
     <List />
-    </div>,
-  document.getElementById('root')
+  </div>,
+  document.getElementById("root")
 );
-
